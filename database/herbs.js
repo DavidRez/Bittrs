271f1b1d6e145ad5c4c7990c02fd2347357b7940
DavidRez <davidraviles@gmail.com>
2018-12-03 21:55:33 -0800

    Array of herbs to be seeded.

---
+++ database/herbs.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {name:'angelica',type: 'strong',benefits:'1,2,3,4,5,6'},
+    {name:'artichoke leaf',type: 'strong',benefits:'7,8,6'},
+    {name:'chamomile',type: 'aromatic',benefits:'9,10,11,20'},
+    {name:'elderflower',type: 'aromatic',benefits:'12,13,11'},
+    {name:'fennel',type: 'aromatic',benefits:'13,6,9,3'},
+    {name:'gentian',type: 'strong',benefits:'14,9'},
+    {name:'juniper',type: 'strong',benefits:'5,1,3,4'},
+    {name:'lavender',type: 'aromatic',benefits:'9,6,11,5,16,4'},
+    {name:'wormwood',type: 'strong',benefits:'17,18'},
+    {name:'mugwort',type: 'strong',benefits:'18,14,19,20'},
+    {name:'cinchona bark',type: 'strong',benefits:'18,1,6,10,4,19'},
+    {name:'orris root',type: 'strong',benefits:'10,14,18'},
+    {name:'hops',type: 'strong',benefits:'10,20'},
+    {name:'lemon peel',type: 'aromatic',benefits:'7,8,21,22'},
+    {name:'horehound',type: 'aromatic',benefits:'2,18,22'},
+    {name:'dill',type: 'aromatic',benefits:'8,23,18,21'},
+    {name:'cumin',type: 'aromatic',benefits:'18,8,23,4'},
+    {name:'cardamom',type: 'aromatic',benefits:'8,5,9,18,21,19,2,7'},
+    {name:'rosemary',type: 'aromatic',benefits:'8,18,18,24,2,8,4,9,6,10'},
+    {name:'mint',type: 'aromatic',benefits:'9,19,18,21'},
+    {name:'rosehip',type: 'aromatic',benefits:'23,9,18,25,5'}
+];
